42553bd75f7151a29b9c5b2a33784fe23aed2702
Malay Nayak <malay.kanta.nayak@gmail.com>
2018-10-28 11:49:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-// function autoload($class){
-//   include 'project/'. $class. '.php';
-// }
-// spl_autoload_register('autoload');
+// php autoloading
+function autoload($class){
+   include 'project/'. $class. '.php';
+}
+spl_autoload_register('autoload');
 
+// composer autoloading
 include 'vendor/autoload.php';
 
 new App\project2\dir\Z();
